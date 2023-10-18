5b421e9b2a08f81d4a5aae5f3ea78bfce2701814
Byoungcheon Lee <lbcsultan@gmail.com>
2023-10-18 18:59:33 +0900

    1st

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
-// import TopicsList from '@/components/TopicsList'
+import TopicsList from '@/components/TopicsList'
 
 export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold">WebDev Topics</h1>
       <p className="mb-4">MongoDB CRUD Example</p>
-      {/* <TopicsList /> */}
+      <TopicsList />
     </>
   )
 }
